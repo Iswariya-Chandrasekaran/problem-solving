a6194be915b18f30509eb7391a1aef7601d2be0a
Iswariya-Chandrasekaran <iswariya169@gmail.com>
2026-04-09 13:23:39 +0530

    candy & str gcd

--- Python/DSA/Array/LC string_gcd.py
+++ Python/DSA/Array/LC string_gcd.py
@@ -1,4 +1,11 @@
 class solution:
+    """
+    Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+    Example 1:
+    Input: str1 = "ABCABC", str2 = "ABC"
+
+    Output: "ABC"
+    """
     def str_hcf(self, str1:str, str2:str)-> str:
       # 1. check is both str follows same pattern if not no gcd exists
       # ABABAB + ABAB == ABAB + ABABAB
